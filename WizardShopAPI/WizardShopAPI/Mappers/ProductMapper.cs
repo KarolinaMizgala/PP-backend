35fa4d604aff97c0bd03f2fcbc2e262a9bb8ae23
BadAga <agnieszka.badocha@gmail.com>
2023-04-13 00:06:50 +0200

    fix: CORS fix, product controller

---
+++ WizardShopAPI/WizardShopAPI/Mappers/ProductMapper.cs
@@ -0,0 +1,25 @@
+﻿using WizardShopAPI.DTOs;
+using WizardShopAPI.Models;
+
+namespace WizardShopAPI.Mappers
+{
+    public class ProductMapper
+    {
+        public static Product ProductDtoToProduct(ref ProductDto dto,ref int productId)
+        {
+            Product mapped = new Product()
+            {
+                Id=productId,
+                Name=dto.Name,
+                Description=dto.Description,
+                Price=dto.Price,
+                Rating=dto.Rating,
+                PhotoId=dto.PhotoId,
+                CategoryId=dto.CategoryId,
+                Popularity=dto.Popularity,
+                Quantity=dto.Quantity
+            };
+            return mapped;
+        }
+    }
+}
--- WizardShopAPI/WizardShopAPI/Models/Product.cs
+++ WizardShopAPI/WizardShopAPI/Models/Product.cs
@@ -12,11 +12,10 @@ public partial class Product
     [Key]
     public int Id { get; set; }
 
-    [StringLength(50)]
+    [StringLength(150)]
     [Unicode(false)]
     public string Name { get; set; } = null!;
 
-    [StringLength(300)]
     [Unicode(false)]
     public string Description { get; set; } = null!;
 
@@ -24,13 +23,13 @@ public partial class Product
 
     [StringLength(50)]
     [Unicode(false)]
-    public string Rating { get; set; } = null!;
+    public string Rating { get; set; } = "No data";
 
     [StringLength(20)]
     [Unicode(false)]
-    public string? IdPhoto { get; set; }
+    public string? PhotoId { get; set; }
 
-    public int IdsKategory { get; set; }
+    public int CategoryId { get; set; }
 
     public int Popularity { get; set; }
 
